415ed428c21a664ab87e0f057d65147a7102409b
Bill Allen <whallen@wellington.com>
2014-06-27 17:16:40 -0400

    make full screen, add forward and braking

--- Speed.elm
+++ Speed.elm
@@ -12,11 +12,13 @@ addV : V -> V -> V
 addV v1 v2 = {x = v1.x + v2.x, y = v1.y + v2.y, z = v1.z + v2.z }
 
 -- Update
-update : (Time, {x: Int, y: Int}) -> State -> State
-update (t, {x,y}) state =
+update : (Time, {x: Int, y: Int}, Bool, Bool) -> State -> State
+update (t, {x,y}, forward, brake) state =
    let
+        newForward = if forward then 1 else 0
+        newBrake = if brake then -1 else 0
         newAccel : V
-        newAccel = {x = toFloat x, y = toFloat y, z = 0}
+        newAccel = {x = toFloat x, y = toFloat y, z = newForward + newBrake}
         newSpeed = addV newAccel state.speed
         newPosition = addV state.position newSpeed
    in
@@ -29,19 +31,22 @@ showV label score =
                    toForm (flow down [plainText label,
                                       show score |> toText |> rightAligned])]
     in
-        collage 200 50 [g]
+          collage 200 50 [g]
 
-display : State -> Element
-display m = collage 400 400 [ circle 10 |> filled blue |> move (m.position.x, m.position.y) ]
+display : Int -> Int -> State -> Element
+display w h m = collage w h [ rect (toFloat w) (toFloat h) |> filled lightGrey,
+                              circle 10 |> filled blue |> move (m.position.x, m.position.y) ]
 
 render : (Int, Int) -> State -> Element
 render (w,h) m = container w h middle (flow down [flow right [showV "Position" m.position,
                                                               showV "Speed" m.speed,
                                                               showV "Accel" m.accel],
-                                                 display m])
+                                                 display w h m])
 input =
-  lift2 (,)
+  lift4 (,,,)
         (foldp (\t acc -> acc + (t/100)) 0 (fps 24))
         Keyboard.arrows
+        Keyboard.space
+        Keyboard.ctrl
 -- Main
 main = lift2 render Window.dimensions <| foldp update m input
